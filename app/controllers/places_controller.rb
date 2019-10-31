4ab021a1bc17d90d6c4af36e9db3fe893adac32d
Liza Cummings <lizacummings@Lizas-iMac.attlocal.net>
2019-10-31 15:11:32 -0500

    file changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,30 +1,17 @@
 class PlacesController < ApplicationController
-<<<<<<< HEAD
    before_action :authenticate_user!, only: [:new, :create]
-=======
->>>>>>> 4ee3109cc31ac8e2a3478b5c8c9a92a145b02abc
-
   def index 
     @places = Place.paginate(page: params[:page], per_page: 3) 
   end
-
   def new
     @place = Place.new
   end
-
   def create
-<<<<<<< HEAD
     current_user.places.create(place_params)
-=======
-    Place.create(place_params)
->>>>>>> 4ee3109cc31ac8e2a3478b5c8c9a92a145b02abc
     redirect_to root_path
   end
-
   private
-
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end
--- config/application.rb
+++ config/application.rb
@@ -1,35 +1,19 @@
-require_relative 'boot'
-
+rrequire_relative 'boot'
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Nomster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-<<<<<<< HEAD
     config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-=======
-      # Add the fonts path
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
->>>>>>> 4ee3109cc31ac8e2a3478b5c8c9a92a145b02abc
-
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
-    
-<<<<<<< HEAD
-
-=======
->>>>>>> 4ee3109cc31ac8e2a3478b5c8c9a92a145b02abc
   end
 end
