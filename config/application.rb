e0f8747b95f5737684d303e3917f5f1e579f9ef1
Liza Cummings <lizacummings@Lizas-iMac.attlocal.net>
2019-11-01 14:32:22 -0500

    lesson 19

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-rrequire_relative 'boot'
+require_relative 'boot'
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
