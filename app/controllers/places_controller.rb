86806ca266174e7e2cd33132309f2853577365d5
Liza Cummings <lizacummings@Lizas-iMac.attlocal.net>
2019-11-22 14:57:46 -0600

    lesson 29

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -28,9 +28,11 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
+    
     @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
